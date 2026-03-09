598b4f3833f0344af75d94f7731a8147a6d9626e
Gabriel Speranceta <gabriel.hesperanceta@gmail.com>
2026-03-09 14:12:24 -0300

    fix: Remove import that dont exist

--- src/features/assessment/index.tsx
+++ src/features/assessment/index.tsx
@@ -1,5 +1,4 @@
 export * from "./pages/AssessmentPage";
-export * from "./pages/AssessmentResultPage";
 export * from "./types";
 export * from "./mocks/index";
 export * from "./components/AssesmentCard";
