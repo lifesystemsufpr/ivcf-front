cc228981fe5d2d62ecacb99bd5eb39b9286402da
Daniel Candido <danielfelipec18@hotmail.com>
2026-04-09 15:05:32 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/ivcf/",
+  base: process.env.BASE_PATH ?? "/ivcf/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
