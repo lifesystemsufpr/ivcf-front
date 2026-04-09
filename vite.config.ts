03188536f2c5a6cf10a521035a5098195ef5af16
Daniel Candido <danielfelipec18@hotmail.com>
2026-04-09 15:10:29 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,31 @@
-import { defineConfig } from "vite";
+
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig({
-  base: process.env.BASE_PATH ?? "/ivcf/",
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/backend": {
-        target: "http://200.236.3.109",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE_PATH ?? "/ivcf/",
+
+    plugins: [react(), tailwindcss()],
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+
+    server: {
+      proxy: {
+        "/backend": {
+          target: "http://200.236.3.109",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
